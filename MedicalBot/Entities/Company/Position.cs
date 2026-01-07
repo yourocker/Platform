35a294bf2b7c68c87366d29275955704fc6dd233
yourocker <awlwlb@yandex.ru>
2026-01-07 08:40:13 +0400

    AddDynamicProperties

--- MedicalBot/Entities/Company/Position.cs
+++ MedicalBot/Entities/Company/Position.cs
@@ -2,10 +2,11 @@
 
 namespace MedicalBot.Entities.Company
 {
-    public class Position
+    public class Position : IHasDynamicProperties
     {
         public Guid Id { get; set; }
         public string Name { get; set; }
         public string? SystemName { get; set; }
+        public string? Properties { get; set; }
     }
 }
--- MedicalBot/Entities/Company/StaffAppointment.cs
+++ MedicalBot/Entities/Company/StaffAppointment.cs
@@ -2,19 +2,16 @@
 
 namespace MedicalBot.Entities.Company
 {
-    public class StaffAppointment
+    public class StaffAppointment : IHasDynamicProperties
     {
         public Guid Id { get; set; }
-        
         public Guid EmployeeId { get; set; }
         public Employee Employee { get; set; }
-
         public Guid DepartmentId { get; set; }
         public Department Department { get; set; }
-
         public Guid PositionId { get; set; }
         public Position Position { get; set; }
-
         public bool IsPrimary { get; set; }
+        public string? Properties { get; set; }
     }
 }
