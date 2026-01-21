1cdab78bcc957c11279a9ce28aebc41ab21ef316
yourocker <awlwlb@yandex.ru>
2026-01-21 12:30:15 +0400

    add new style, refactor _Layout

---
+++ CRM/Models/NavigationViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Core.Entities.Platform;
+
+namespace CRM.Models
+{
+    public class NavigationViewModel
+    {
+        public List<AppCategory> CustomCategories { get; set; } = new();
+        public AppCategory CrmCategory { get; set; }
+        public string Controller { get; set; }
+        public string Action { get; set; }
+        public string EntityCode { get; set; }
+
+        // Состояния активности разделов
+        public bool IsCRMActive { get; set; }
+        public bool IsCompanyActive { get; set; }
+        public bool IsTasksActive { get; set; }
+        public bool IsSettingsActive { get; set; }
+    }
+}
