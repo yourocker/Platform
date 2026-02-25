16eaac1fc6169e2fbd3c2796b91c171600004b94
yourocker <awlwlb@yandex.ru>
2026-02-25 14:38:57 +0400

    UnversionedFiles

---
+++ Core/Migrations/20260212214717_UpdateFormDesignerInfrastructure.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateFormDesignerInfrastructure : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "AppFormDefinitions",
+                type: "character varying(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AppFormDefinitions_AppDefinitionId_Type_Name",
+                table: "AppFormDefinitions",
+                columns: new[] { "AppDefinitionId", "Type", "Name" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_AppFormDefinitions_AppDefinitionId_Type_Name",
+                table: "AppFormDefinitions");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "AppFormDefinitions");
+        }
+    }
+}
